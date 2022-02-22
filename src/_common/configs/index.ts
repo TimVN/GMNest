c7d6d040c6e694ae056e811881304589a90fe77c
TimVN <tim.van.norde1993@gmail.com>
2022-02-22 19:19:05 +0100

    Add env file

--- src/_common/configs/index.ts
+++ src/_common/configs/index.ts
@@ -1,17 +1,7 @@
 import database from './database';
-import { Logger } from '@nestjs/common';
-
-const logger = new Logger('Config');
 
 export const getConfigFiles = () => {
-  if (process.env.HEROKU_APP) {
-    return [];
-  }
-
-  logger.log('Environment Variables');
-  logger.log(Object.keys(process.env));
-
-  return ['.env.development'];
+  return ['.env.development', '.env'];
 };
 
 export const configs = [database];
