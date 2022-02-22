333812f3f2e29663bd0ec4c7d46628349ea32745
TimVN <tim.van.norde1993@gmail.com>
2022-02-22 02:20:54 +0100

    Add logger

--- src/_common/configs/index.ts
+++ src/_common/configs/index.ts
@@ -1,11 +1,15 @@
 import database from './database';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Config');
 
 export const getConfigFiles = () => {
   if (process.env.HEROKU_APP) {
     return [];
   }
 
-  console.log(process.env);
+  logger.log('Environment Variables');
+  logger.log(process.env);
 
   return ['.env.development'];
 };
