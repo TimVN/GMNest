56744c1c35b919c09cd88828e26a18126705fca1
TimVN <tim.van.norde1993@gmail.com>
2022-02-22 02:09:44 +0100

    Remove fallback values

--- src/_common/configs/database.ts
+++ src/_common/configs/database.ts
@@ -2,8 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   redis: {
-    host: process.env.REDIS_HOST || 'localhost',
-    password: process.env.REDIS_PASSWORD || null,
-    port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
+    port: process.env.REDIS_PORT,
   },
 }));
