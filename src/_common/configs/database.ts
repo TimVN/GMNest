d63531270f022c6501f64ff5ea2507432e6bc55d
TimVN <tim.van.norde1993@gmail.com>
2022-02-22 01:24:39 +0100

    Add fallback values to redis config

--- src/_common/configs/database.ts
+++ src/_common/configs/database.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
   },
 }));
